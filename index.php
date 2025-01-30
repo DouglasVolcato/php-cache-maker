d6584f540cbdd5525e6e1af319680e98a58f5d26
DouglasVolcato <douglasvolcato@gmail.com>
2025-01-30 00:39:54 -0300

    chore: implement test case

--- index.php
+++ index.php
@@ -1,11 +1,18 @@
 <?php
 
+// require_once './helpers/cacheMakerHelper.php';
+// require_once './slow_process.php';
+
+// $process = new SlowProcess();
+
+// echo json_encode($process->getData('123', 'Teste', 23, ['test' => 'test']));
+
+///////////////////////////////////////////
+
 require_once './helpers/cacheMakerHelper.php';
 require_once './slow_process.php';
 
 $process = new SlowProcess();
-
 $cache = new CacheMakerHelper('relatorio_teste', 'cache/relatorio_teste.db', 1);
-// $data = json_encode($process->getData('123', 'Teste', 23, ['test' => 'test']));
-$data = json_encode($cache->execute([$process, 'getData'], ['123', 'Teste', 1231, ['test' => 'te2st'], true]));
-echo $data;
+
+echo json_encode($cache->execute([$process, 'getData'], ['123', 'Teste', 1231, ['test' => 'te2st'], true]));
